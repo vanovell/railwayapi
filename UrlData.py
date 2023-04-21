58065f0bfb1025f042e81b21528dbb4cf81707a8
pr0f-moriarty <drunkpegasus@protonmail.com>
2023-04-21 20:01:29 +0530

    up

---
+++ UrlData.py
@@ -0,0 +1,23 @@
+
+
+from pydantic import BaseModel
+
+class UrlData(BaseModel):
+    """
+    define your variables here with same name
+    that you want your user to see.
+    """
+
+    url : str
+
+
+
+
+
+
+
+
+
+
+
+
